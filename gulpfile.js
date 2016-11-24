f0ea1410b8b54ce7e1c08f376c7f337af1e8b254
Andreas Santarsieri <santarsieri@gmail.com>
2016-11-24 18:23:33 +0100

    test only

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('css/screen.scss')
+  return gulp.src('css/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('css/screen.scss', ['sass']);
+  gulp.watch('css/*.scss', ['sass']);
 });
