539aaebbcb8f2fa973483216fdedaf53603cbd84
Andreas Santarsieri <santarsieri@gmail.com>
2016-12-01 19:11:12 +0100

    added css sourcemaps, adaption of gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 'use strict';
- 
+
+// require = load plugin
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps'); // added for sourcemaps
  
 gulp.task('sass', function () {
   return gulp.src('css/*.scss')
+  	.pipe(sourcemaps.init()) // added for sourcemaps
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('/maps')) //added for sourcemaps
     .pipe(gulp.dest('css'));
 });
  
@@ -15,7 +19,7 @@ gulp.task('sass:watch', function () {
   	// gulp.watch('css/screen.scss', ['sass']);
   	// gulp.watch('css/partials/*.scss', ['sass']);
 
-  	// better
+  	//  better
   	gulp.watch(['css/*.scss', 'css/partials/*.scss'], ['sass']);
 
 });
